d5d80f7c9c95b908741927a8323eaffa93152596
Matthew Pierce <lesbianhaircut@outlook.com>
2021-09-02 18:11:59 -0400

    c

--- script.js
+++ script.js
@@ -3,10 +3,26 @@ window.onload = () => {
   const button = document.querySelector(".icon-container");
   const buttonIcon = document.querySelector(".circle-icon");
   const prices = document.querySelectorAll(".price");
+  const plans = document.querySelectorAll(".plan");
   const monthlyPrices = ["19.99", "24.99", "39.99"];
   const annualPrices = ["199.99", "249.99", "399.99"];
+  const learnMorebuttons = document.querySelectorAll(".learn-more");
 
   // Functions
+
+  function clearSelected() {
+    plans.forEach((plan) => {
+      plan.classList.remove("selected-plan");
+    });
+  }
+
+  function selectPlan(button) {
+    clearSelected();
+    const regex = /[0-9]/g;
+    let planIndex = button.id.match(regex);
+    planIndex = parseInt(planIndex - 1);
+    plans[planIndex].classList.add("selected-plan");
+  }
 
   function setAnnual() {
     prices.forEach((planPrice, index) => {
@@ -32,4 +48,9 @@ window.onload = () => {
 
   // Event listeners
   button.addEventListener("click", annualToggle);
+  learnMorebuttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      selectPlan(button);
+    });
+  });
 };
